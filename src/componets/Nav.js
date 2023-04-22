f9df38392d9cf6de6eca72d1af1a3ddeba01d283
Damien Nelson <dhnisaman@gmail.com>
2023-04-22 15:49:02 -0400

    add styling to nav bar

--- src/componets/Nav.js
+++ src/componets/Nav.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom'
 
 function Nav() {
   return (
-    <div>
+    <div className='flex w-full h-10 bg-red-500 mb-10'>
         {/* <NavLink to='/companies'>Companies</NavLink>
         <NavLink to='/drinks'>Drinks</NavLink> */}
-        <NavLink to='/home'>Home</NavLink>
+        <NavLink to='/home' className='bold text-black text-2xl'>Home</NavLink>
     </div>
   )
 }
