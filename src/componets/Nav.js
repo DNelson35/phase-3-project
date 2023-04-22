5b7867e21900893c84c9a11b6ad7f15f9ab601b7
Damien Nelson <dhnisaman@gmail.com>
2023-04-21 23:36:52 -0400

    set up nav bar component and give navigation

---
+++ src/componets/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Nav() {
+  return (
+    <div>
+        {/* <NavLink to='/companies'>Companies</NavLink>
+        <NavLink to='/drinks'>Drinks</NavLink> */}
+        <NavLink to='/home'>Home</NavLink>
+    </div>
+  )
+}
+
+export default Nav
