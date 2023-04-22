5c3b4fc1550e5b153c3e09a45e23bbbae12d1596
Damien Nelson <dhnisaman@gmail.com>
2023-04-21 23:36:26 -0400

    create root element for react app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+
+const el = document.getElementById('root')
+
+const root = ReactDOM.createRoot(el)
+
+
+root.render(
+    <Router >
+        <App />
+    </Router>
+)
