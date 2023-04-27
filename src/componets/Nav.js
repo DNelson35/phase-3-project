4b50e1a285f11649872306a009b6294c34e89cc9
Damien Nelson <dhnisaman@gmail.com>
2023-04-27 10:23:28 -0400

    add link to the edit page

--- src/componets/Nav.js
+++ src/componets/Nav.js
@@ -4,9 +4,8 @@ import { NavLink } from 'react-router-dom'
 function Nav() {
   return (
     <div className='flex w-full h-10 bg-red-500 mb-10'>
-        {/* <NavLink to='/companies'>Companies</NavLink>
-        <NavLink to='/drinks'>Drinks</NavLink> */}
         <NavLink to='/' className='bold text-black text-2xl'>Home</NavLink>
+        <NavLink to='/edit' className='bold text-black text-2xl'>Edit</NavLink>
     </div>
   )
 }
