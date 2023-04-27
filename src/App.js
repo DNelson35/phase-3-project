4e97de98bbf1ddd192f02a11e344acb342924932
Damien Nelson <dhnisaman@gmail.com>
2023-04-27 10:22:59 -0400

    add companies, delete drinks state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import {useState, useEffect} from 'react'
 import Nav from './componets/Nav'
-import Drinks from './pages/Drinks'
-import Companies from './pages/company'
 import Home from './pages/Home'
+import Drinks from './pages/Drinks'
+import Company from './pages/company'
+import Edit from './pages/Edit'
 
 
 function App() {
   const [companies, setCompanies] = useState([])
-  const [drinks, setDrinks] = useState([])
+  
 
   useEffect(() => {
     fetch("http://localhost:9292/companies")
@@ -17,13 +18,15 @@ function App() {
     .then(companies => setCompanies(companies))
   },[])
 
+
   return (
     <div>
       <Nav /> 
       <Routes>
-        <Route path='/company/:id' element={<Companies companies={companies} setCompanies={setCompanies} drinks={drinks} setDrinks={setDrinks}/>}/>
-        <Route path='/drink/:id' element={<Drinks/>}/>
-        <Route path='/' element={<Home companies={companies} setCompanies={setCompanies}/>}/>
+        <Route path='/company/:id' element={<Company companies={companies} setCompanies={setCompanies}/>}/>
+        <Route path='/company/:id/drinks' element={<Drinks/>}/>
+        <Route path='/edit' element={<Edit companies={companies} setCompanies={setCompanies}/>}/>
+        <Route path='/' element={<Home companies={companies}/>}/>
       </Routes>
     </div>
   )
