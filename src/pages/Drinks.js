5111a1434e3f2f184ce19a292e8dbfcb0a5051ca
Damien Nelson <dhnisaman@gmail.com>
2023-04-27 10:26:04 -0400

    add use location and return value for drinks

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 function Drinks() {
+  const location = useLocation()
+  const drinks = location.state
+
+  const drinksList = drinks.map(drink => (
+    <div key={drink.id}>
+      <h1>{drink.name}</h1>
+      <img src={drink.image_url} alt='drink'/>
+      <p>{drink.description}</p>
+    </div>
+   ))
   return (
-    <div>Drinks</div>
+    <div>
+      {drinksList}
+    </div>
   )
 }
 
